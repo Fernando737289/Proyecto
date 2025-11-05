32902669b8869542fb84924bdec9a205ae06687e
yo <yo@DESKTOP-48HH0TK>
2025-11-05 13:17:45 -0300

    fix: elimine una funcion inservible

--- Proyecto/Form2.vb
+++ Proyecto/Form2.vb
@@ -3,10 +3,6 @@
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
-    End Sub
-
 
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.BackColor = Color.Gray
